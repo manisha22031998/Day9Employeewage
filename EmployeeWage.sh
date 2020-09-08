0541c12ff0948400bdef67b72bdd01cbeb120f43
unknown <manishasingh2203@gmail.com>
2020-09-08 12:32:15 +0530

    merged

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,25 @@
 echo welcome
-#UC1
 echo to daily wage computation
+#UC1
+for (( day=1;day<=20;day++ ))
+do
+	attendance=$((RANDOM%2))
+	if [ $attendance -eq 0 ]
+	then
+		emp=0
+		echo absent
+	else
+		emp=1
+		echo present
+	fi
+done
+#UC2
+if [ $emp -eq 1 ]
+then
+	wph=20
+	hr=8
+	dailywage=$((wph*hr))
+	echo $dailywage
+else
+	echo Working hr is 0
+
